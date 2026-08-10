60b958e35cec33d3d228730233caea1992283b3b
datf <5156893+datf@users.noreply.github.com>
2026-08-10 13:56:58 +0200

    remove unused variables

--- dante/rootfs/usr/bin/dante-server.sh
+++ dante/rootfs/usr/bin/dante-server.sh
@@ -2,8 +2,6 @@
 
 bashio::log.debug 'Preparing dante...'
 
-CUSTOM_CADDYFILE_PATH="${XDG_CONFIG_HOME}/sockd.conf"
-
 # TODO: check for config and use user-defined config rather than /etc/sockd.conf
 
 bashio::log.info "Runing dante..."
