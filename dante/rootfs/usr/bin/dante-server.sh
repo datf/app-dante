693666bd693561d97812be2e601f8a3aa0f7f7fa
datf <5156893+datf@users.noreply.github.com>
2026-08-13 13:10:45 +0200

    custom config

--- dante/rootfs/usr/bin/dante-server.sh
+++ dante/rootfs/usr/bin/dante-server.sh
@@ -1,8 +1,31 @@
 #!/command/with-contenv bashio
+set -euo pipefail
 
-bashio::log.debug 'Preparing dante...'
+USER_CONFIG_PATH="${XDG_CONFIG_HOME:-/config}/sockd.conf"
+DEFAULT_CONFIG="/etc/sockd.conf"
 
-# TODO: check for config and use user-defined config rather than /etc/sockd.conf
+bashio::log.debug "Preparing dante..."
 
-bashio::log.info "Runing dante..."
-exec sockd
+if [[ -f "${USER_CONFIG_PATH}" ]]; then
+    CONFIG_PATH="${USER_CONFIG_PATH}"
+else
+    CONFIG_PATH="${DEFAULT_CONFIG}"
+fi
+
+if [[ ! -f "${CONFIG_PATH}" ]]; then
+    bashio::exit.nok "Dante config not found: ${CONFIG_PATH}"
+fi
+
+if [[ ! -r "${CONFIG_PATH}" ]]; then
+    bashio::exit.nok "Can't read dante config: ${CONFIG_PATH}"
+fi
+
+bashio::log.info "Validating ${CONFIG_PATH}..."
+
+if ! sockd -V -f "${CONFIG_PATH}"; then
+    bashio::exit.nok "Invalid configuration."
+fi
+
+bashio::log.info "Starting Dante..."
+
+exec sockd -f "${CONFIG_PATH}" -N 1
